20e0193a48a369066c427df392a3e51d73c93642
shreeyasatam <shreeyasatam@yahoo.co.in>
2023-02-14 11:01:56 +1100

    route added

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ const articleSchema = {
 
 const Article = mongoose.model("Artcle", articleSchema);
 
-app.get("/articles", function(req, res){
+app.route("/articles")
+.get(function(req, res){
     Article.find(function(err, foundArticles){
         if(!err){
             res.send(foundArticles)
@@ -36,8 +37,7 @@ app.get("/articles", function(req, res){
         console.log(foundArticles);
     });
 })
-
-app.post("/articles", function(req, res){
+.post(function(req, res){
     const newArticle = new Article({
         title: req.body.title,
         content: req.body.content
@@ -51,6 +51,16 @@ app.post("/articles", function(req, res){
         }
     });
 })
+.delete(function(req, res){
+    Article.deleteMany(function(err){
+        if(!err){
+            res.send("Successfully deleted")
+        } else{
+            res.send("there is an error")
+        }
+    });
+})
+
 
 app.listen(3000, function(){
     console.log("server started on port 3000")
